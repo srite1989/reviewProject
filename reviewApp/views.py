e04eb6d127a70d7b3f9b6bfdb75ade0127d49e0e
deepu.lucky157@gmail.com <LakshmiDeepika@1989>
2022-09-09 05:32:33 -0500

    intial commit

---
+++ reviewApp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+class HomePageView(TemplateView):
+    template_name='home.html'
+
+class RegisterPageView(TemplateView):
+    template_name='register.html'  
+
+class LoginPageView(TemplateView):
+    template_name='login.html'      
