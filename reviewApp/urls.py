51f54d84778e54062deb949a7bda7212f107a7b7
eeswardutta <raonani23@yahoo.co.in>
2022-09-12 16:16:23 +0530

    commit by eeswar on 09-12-22 at 16:15 P.M

--- reviewApp/urls.py
+++ reviewApp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView,RegisterPageView
 
 urlpatterns=[
-    path('',HomePageView.as_view(),name='home')
+    path('',HomePageView.as_view(),name='home'),
+    path('showRegister/',RegisterPageView.as_view(),name='register')
 ]
